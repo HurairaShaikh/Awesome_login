8fd927fceb314d5725ae91c11428f70329ce6577
Huraira Shaikh <hurairashiekh076@gmail.com>
2026-03-17 06:51:26 +0500

    improved

--- lib/local/home.dart
+++ lib/local/home.dart
@@ -42,11 +42,7 @@ class _HomeState extends State<Home> {
         ],
       ),
       body: Center(
-        child: Text(
-          currentUser!.email.toString() +
-              "\n" +
-              currentUser!.displayName.toString(),
-        ),
+        child: Text("Hi ${currentUser!.displayName??""}\n${currentUser!.email}"),
       ),
     );
   }
